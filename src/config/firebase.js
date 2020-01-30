2b43e0b8a5a779667bfa4e96f772d1ae9446de3e
knoxgon <volkan.guven@hotmail.se>
2020-01-30 12:00:06 +0100

    Added firebase config file

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-CcDm3mp0BiBY4HM3e4HBkl9POeFkpBA",
+  authDomain: "fe19tp2cherry.firebaseapp.com",
+  databaseURL: "https://fe19tp2cherry.firebaseio.com",
+  projectId: "fe19tp2cherry",
+  storageBucket: "fe19tp2cherry.appspot.com",
+  messagingSenderId: "73963246117",
+  appId: "1:73963246117:web:637e5993475c5330b3e5cc",
+  measurementId: "G-7EYPWPKBM5"
+};
+
+export default firebase.initializeApp(firebaseConfig);
