8bd1aa062f9fca26a3ca1fffabf774bbe96a1fc5
klippdocka <elina.larsson@student.kyh.se>
2020-01-31 09:34:08 +0100

    Moved color to theme file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from "./assets/logo_transparent.png";
-
+import Theme from "./config/theme";
+import * as IconesSolid from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const StyledApp = styled.div`
 width:100%;
@@ -12,7 +14,7 @@ const Nav = styled.nav`
 width:100%;
 height:13rem;
 display:flex;
-background-color:#e6e6e6;
+background-color:${Theme.colors.greyLight};
 `;
 
 const Ul = styled.ul`
@@ -23,11 +25,16 @@ align-items:center;
 margin:0;
 padding:0;
 overflow:hidden;
+@media screen and (max-width: 600px) {
+    float: none;
+    width: 100%;
+  
+}
 
 `;
 
 const Li = styled.li`
-flex:0.7;
+margin-left:2rem;
 color:black;
 text-align:center;
 padding: 2rem 2rem;
@@ -45,11 +52,11 @@ text-decoration:none;
 `;
 
 const LiAccount = styled.li`
-flex:3;
-text-align:end;
-padding: 2rem 6rem;
+text-align:center;
+padding: 5rem 10rem;
 font-size:2rem;
 text-decoration:none;
+display:block;
 
 `;
 
@@ -67,13 +74,20 @@ justify-content:center;
 bottom:0;
 text-align:center;
 height:5rem;
-background-color:#e6e6e6;
+background-color:${Theme.colors.greyLight};
 `;
 
 const P = styled.p`
 padding:4rem;
 font-size:1.8rem;
 `;
+
+const Menu = styled(FontAwesomeIcon)`
+margin-top:4.5rem;
+margin-right:3rem;
+font-size:3.5rem;
+`;
+
 
 
 
@@ -93,9 +107,10 @@ class App extends React.Component {
             <Li><A className="active" href="#home">Home</A></Li>
             <Li><A href="">Solutions</A></Li>
             <Li><A href="">About</A></Li>
-            <LiAccount><A href="">Account</A></LiAccount>
           </Ul>
-
+            <LiAccount><A href="">Account</A></LiAccount>
+           
+            
         </Nav>
 
         <Footer>
---
+++ src/config/theme.js
@@ -0,0 +1,14 @@
+const Theme = {
+    colors: {
+        greyLight:'#e6e6e6'
+    },
+
+    screenSize: {
+        xsmall: '600px',
+        small: '768px',
+        medium: '992px',
+        large: '1200px'
+    }
+}
+
+export default Theme;
