746e4d531bb897e964838b656a7e1de4447e7b56
klippdocka <elina.larsson@student.kyh.se>
2020-02-02 20:32:50 +0100

    moved footer to a own file. Added som style in footer.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import Theme from "./config/theme";
 import Header from "./header";
+import Footer from "./footer";
 
 
 const StyledApp = styled.div`
 width:100%;
 height:100%;
 `;
-
-const Footer = styled.footer`
-width:100%;
-display:flex;
-position:fixed;
-align-items:center;
-justify-content:center;
-bottom:0;
-text-align:center;
-height:5rem;
-background-color:${Theme.colors.greyLight};
-`;
-
-const P = styled.p`
-padding:4rem;
-font-size:1.8rem;
-`;
-
-
-
 
 
 class App extends React.Component {
@@ -40,10 +21,7 @@ class App extends React.Component {
        
   {<Header/>}
   
-  <Footer>
-      <P>Stockholm - Sweden 131 42</P> <P>Long(59.31), Lat(18.20)</P>
-    </Footer>
-
+  {<Footer/>}
       </StyledApp>
     );
   }
