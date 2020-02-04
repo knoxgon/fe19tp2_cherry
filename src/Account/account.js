4462d4c7f1bc3c0e46fc64aaad5a6b666155a27e
klippdocka <elina.larsson@student.kyh.se>
2020-02-04 22:14:48 +0100

    started with the admin view

--- src/Account/account.js
+++ src/Account/account.js
@@ -1,17 +1,61 @@
 import React from 'react';
+import SparbankenLogo from "../Images/new-swedbank-transparent.png";
+import AddUser from "../Images/add-user.png";
+import * as IconesSolid from '@fortawesome/free-solid-svg-icons';
+import ApiIcon from '../Images/API-icon.png'
+
+
 import {
-  Wrapper
+  Wrapper, 
+  SparbankenImg,
+  Styleddiv,
+  StyledH2,
+  StyledAddUser,
+  UserFunctionDiv,
+  StyledP,
+  StyledGraph,
+  StyledLogout,
+  LogoutDiv,
+  StyledApiIcon,
+  StyledPtag
+  
 } from './styledAccount';
+
+
 
 class Account extends React.Component {
 
   render() {
     return (
       <Wrapper>
-        <div>
+            
+         <Styleddiv>
+         <SparbankenImg src ={SparbankenLogo}/>
+         <StyledH2>Fredrik Åhlberg</StyledH2>
           
-          <h2>Fredrik Åhlberg</h2>
-        </div>
+         
+           </Styleddiv>
+
+      <UserFunctionDiv>
+        <StyledAddUser src = {AddUser} />
+        <StyledP>Add user</StyledP>
+      </UserFunctionDiv>
+
+      <UserFunctionDiv>
+      <i><StyledGraph icon={IconesSolid.faChartBar}/></i>
+      <StyledP>Generate graph</StyledP>
+      </UserFunctionDiv>
+
+      <UserFunctionDiv>
+      <i><StyledApiIcon src = {ApiIcon}/></i>
+      <StyledPtag>Get data</ StyledPtag>
+      </UserFunctionDiv>
+
+
+      <LogoutDiv>
+      <i><StyledLogout icon={IconesSolid.faSignOutAlt}/></i>
+      <StyledP>Sign out</StyledP>
+      </LogoutDiv>
 
       </Wrapper>
     );
--- src/Account/styledAccount.js
+++ src/Account/styledAccount.js
@@ -1,12 +1,122 @@
 import styled from 'styled-components';
 import Theme from "../config/theme";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Wrapper = styled.div`
 
 width:100%;
 height:100%;
+
+
 `;
 
+const SparbankenImg = styled.img`
+margin: 3rem 1rem 3rem 1rem;
+justify-content:center;
+width:9rem;
+height:auto;
+`;
+
+const Styleddiv = styled.div`
+margin-bottom:5rem;
+display:flex;
+align-items:center;
+
+
+`;
+
+const StyledH2 = styled.h2`
+
+margin-left:0rem;
+font-size:1.5rem;
+
+`;
+
+const StyledAddUser = styled.img`
+
+ margin-left:4rem;
+ width:4rem;
+ &:hover {
+    transform: scale(1.1);
+
+ }
+`;
+
+const UserFunctionDiv = styled.div`
+display:flex;
+align-items:center;
+`;
+
+const StyledP = styled.p`
+font-size:2rem;
+font-weight:bold;
+margin-block-start: 0em;
+margin-block-end: 0em;
+margin-left:2rem;
+
+   
+`;
+
+const StyledGraph = styled(FontAwesomeIcon)`
+font-size:3.5rem;
+margin: 3rem 0 3rem 4.2rem;
+&:hover {
+    transform: scale(1.1);
+
+ }
+
+`;
+const StyledLogout = styled(FontAwesomeIcon)`
+font-size:3.5rem;
+margin: 3rem 0 0 4.4rem;
+&:hover {
+    transform: scale(1.1);
+
+ }
+
+`;
+
+const LogoutDiv = styled.div`
+display:flex;
+text-align:center;
+align-items:flex-end;
+height:28rem;
+`;
+
+
+const StyledApiIcon = styled.img`
+
+ margin-left:3.5rem;
+ width:5rem;
+
+ &:hover {
+    transform: scale(1.1);
+
+ }
+`;
+
+const StyledPtag = styled.p`
+font-size:2rem;
+font-weight:bold;
+margin-block-start: 0em;
+margin-block-end: 0em;
+margin-left:1.3rem;
+`;
+
+
+
 export {
-    Wrapper
+    Wrapper,
+    SparbankenImg,
+    Styleddiv,
+    StyledH2,
+    StyledAddUser,
+    UserFunctionDiv,
+    StyledP,
+    StyledGraph, 
+    StyledLogout,
+    LogoutDiv,
+    StyledApiIcon,
+    StyledPtag
+    
 }
