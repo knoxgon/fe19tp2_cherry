017b2bc39ff20789a6476d2ee0789c9133b744a2
knoxgon <volkan.guven@hotmail.se>
2020-02-04 18:31:47 +0100

    Moved routing back to App and added partial protocols according authentication

--- src/App.js
+++ src/App.js
@@ -2,21 +2,60 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from "./Header/header";
 import Footer from "./Footer/footer";
-import Routes from './routes';
-
+import PrivateRoute from './__route/private';
+import Account from './Account/account';
+import Login from './Login/login';
+import { Route, Switch } from 'react-router-dom'
+import fireapp from './config/firebase'
 
 const StyledApp = styled.div`
+
   width:100%;
   height:100%;
 `;
 
 
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      loading: false,
+      authenticated: false,
+      currentUser: null
+    }
+  }
+  
+  componentWillMount() {
+    fireapp.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          currentUser: user,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          currentUser: null,
+          loading: false
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <StyledApp>
         <Header />
-        <Routes></Routes>
+        <Switch>
+          <Route exact path="/" />
+          <PrivateRoute exact path="/account" authenticated={this.state.authenticated} component={Account} />
+          <PrivateRoute exact path="/solutions" />
+          <Route path="/login" component={Login} />
+          <Route path="/about" />
+        </Switch>
         <Footer />        
       </StyledApp>
     );
