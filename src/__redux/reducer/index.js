fdebb70a74ecb327e3b757daf981f3c6046fb18e
knoxgon <volkan.guven@hotmail.se>
2020-02-06 10:07:05 +0100

    Attached the authentication reducer with the core firebase reducer

---
+++ src/__redux/reducer/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import {firebaseReducer} from 'react-redux-firebase';
+import authReducer from './auth';
+
+export default combineReducers({
+  firebaseReducer,
+  authReducer
+});
