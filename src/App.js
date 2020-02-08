1a5e40f36bbd652610800964f153129814de853a
knoxgon <volkan.guven@hotmail.se>
2020-02-08 14:59:32 +0100

    Protected route is reconstructed. Routers are moved to own file

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from "./Navbar/navbar";
 import Footer from "./Footer/footer";
-// import PrivateRoute from './__route/private';
-import Account from './Account/account';
-import Login from './Login/login';
-import { Route, Switch } from 'react-router-dom'
 import { firebase } from './__config/firebase'
-import User from "./user/user";
+import Routes from './__route/routes';
 
 const StyledApp = styled.div`
-
   width:100%;
   height:100%;
 `;
-
-
 
 class App extends React.Component {
   constructor(props) {
@@ -48,14 +41,7 @@ class App extends React.Component {
     return (
       <StyledApp>
         <Navbar />
-        <Switch>
-          <Route exact path="/" />
-          <Route exact path="/account" component={Account} />
-          <Route exact path="/solutions" />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/about" />
-          <Route path="/user" component={User} />
-        </Switch>
+        <Routes></Routes>
         <Footer />        
       </StyledApp>
     );
