fd8fc235c9475e949accd7144ce96b91087632d8
knoxgon <volkan.guven@hotmail.se>
2020-02-08 13:51:55 +0100

    Temporary reset of route navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from "./Header/header";
 import Footer from "./Footer/footer";
-import PrivateRoute from './__route/private';
+// import PrivateRoute from './__route/private';
 import Account from './Account/account';
 import Login from './Login/login';
 import { Route, Switch } from 'react-router-dom'
-import fireapp from './config/firebase'
+import { firebase } from './__config/firebase'
 import User from "./user/user";
 
 const StyledApp = styled.div`
@@ -27,7 +27,7 @@ class App extends React.Component {
       currentUser: null
     }
 
-    fireapp.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setState({
           authenticated: true,
@@ -50,10 +50,10 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" />
-          <PrivateRoute exact path="/account" authenticated={this.state.authenticated} component={Account} />
-          <PrivateRoute exact path="/solutions" />
-          <Route path="/login" component={Login} />
-          <Route path="/about" />
+          <Route exact path="/account" component={Account} />
+          <Route exact path="/solutions" />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/about" />
           <Route path="/user" component={User} />
         </Switch>
         <Footer />        
