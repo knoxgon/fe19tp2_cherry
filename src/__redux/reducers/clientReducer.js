29317d2210273ec90a9582ff6eba3dd0a957a4c2
knoxgon <volkan.guven@hotmail.se>
2020-02-09 22:46:42 +0100

    Client reducers instantiated

---
+++ src/__redux/reducers/clientReducer.js
@@ -0,0 +1,23 @@
+import { ADD_CLIENT_SUCCESS, ADD_CLIENT_FAILURE } from "../actions/types";
+
+
+const initialState = {
+  feedback: null
+}
+
+export const clientReduder = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_CLIENT_SUCCESS:
+      return {
+        ...state,
+        feedback: null
+      }
+    case ADD_CLIENT_FAILURE:
+      return {
+        ...state,
+        feedback: action.payload
+      }
+    default:
+      return state;
+  }
+}
