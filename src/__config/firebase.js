efbfc19b341a7596417cb77e59b6f3b11fb81ea7
knoxgon <volkan.guven@hotmail.se>
2020-02-09 14:40:17 +0100

    Removed old configuration

--- src/__config/firebase.js
+++ src/__config/firebase.js
@@ -12,14 +12,6 @@ const fireconf = {
     messagingSenderId: "983217516666",
     appId: "1:983217516666:web:dd697cd6277bb136898bc3",
     measurementId: "G-FVVGB6XQTN"
-  // apiKey: "AIzaSyC-CcDm3mp0BiBY4HM3e4HBkl9POeFkpBA",
-  // authDomain: "fe19tp2cherry.firebaseapp.com",
-  // databaseURL: "https://fe19tp2cherry.firebaseio.com",
-  // projectId: "fe19tp2cherry",
-  // storageBucket: "fe19tp2cherry.appspot.com",
-  // messagingSenderId: "73963246117",
-  // appId: "1:73963246117:web:637e5993475c5330b3e5cc",
-  // measurementId: "G-7EYPWPKBM5"
 };
 
 firebase.initializeApp(fireconf)
