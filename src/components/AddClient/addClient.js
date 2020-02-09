3e228b9da268718cb78c7a320c7f4f8ba9422b85
knoxgon <volkan.guven@hotmail.se>
2020-02-09 22:47:09 +0100

    Client component implemented

---
+++ src/components/AddClient/addClient.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addClient } from '../../__redux/actions/clientActions';
+
+const AddClient = (props) => {
+  const [info, setInfo] = useState({ email: '', password: '', firstname: '', lastname: '', role: '', company: '', plan: '', period: '' })
+
+  const onClientRegistration = (e) => {
+    e.preventDefault();
+    props.makeClient(info);
+  }
+
+  const onInputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInfo({...info, [name]: value});
+  }
+
+  return (
+      <form onSubmit = {onClientRegistration}>
+        <input placeholder="Email" type="email" name="email" onChange={onInputChangeHandler} autoComplete="new-password"></input>
+        <br/>
+        <input placeholder="Password" type="password" name="password" onChange={onInputChangeHandler}></input>
+        <br/>
+        <input placeholder="Firstname" type="text" name="firstname" onChange={onInputChangeHandler}></input>
+        <br/>
+        <input placeholder="Lastname" type="text" name="lastname" onChange={onInputChangeHandler}></input>
+        <br/>
+        <input placeholder="Role" type="text" name="role" onChange={onInputChangeHandler}></input>
+        <br/>
+        <input placeholder="Company" type="text" name="company" onChange={onInputChangeHandler}></input>
+        <br/>
+        <input placeholder="Plan" type="text" name="plan" onChange={onInputChangeHandler}></input>
+        <br/>
+        <input placeholder="Period" type="text" name="period" onChange={onInputChangeHandler}></input>
+        <br/>
+        {props.feedback ? <div>Message: {props.feedback}</div> : null }
+        <br/>
+        <button type="submit">Add</button>
+      </form>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    feedback: state.client.feedback
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    makeClient: (clientinfo) => dispatch(addClient(clientinfo))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddClient)
