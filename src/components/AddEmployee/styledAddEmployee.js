9f4e330f585ede44af1ecb6b6146f1411f148914
fabbethoma <55735538+fabbethoma@users.noreply.github.com>
2020-02-11 12:21:19 +0100

    added styling for employee form in mobile-view

---
+++ src/components/AddEmployee/styledAddEmployee.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import Theme from '../../__config/theme';
+
+
+const EmployeeForm = styled.form`
+
+
+@media screen and (max-width: ${Theme.screenSize.xsmall}){
+
+    input{
+        width: 90%;
+        margin-left: 1rem;
+        margin-bottom: 1rem;
+        
+    }
+
+    button{
+        display: block;
+        margin: auto;
+    }
+}
+
+`;
+
+export {
+    EmployeeForm
+}
