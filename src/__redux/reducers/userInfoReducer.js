a6a47315d9db692a4901596cfb7d107f6d4afe9d
jbrorson <josefine.brorson@gmail.com>
2020-02-12 11:52:13 +0100

    Added new properties to reducer

--- src/__redux/reducers/userInfoReducer.js
+++ src/__redux/reducers/userInfoReducer.js
@@ -1,28 +1,33 @@
-import { USER_INFO_FETCH_SUCCESS, USER_INFO_FETCH_FAILURE } from "../actions/types";
+import {
+  USER_INFO_FETCH_SUCCESS,
+  USER_INFO_FETCH_FAILURE
+} from "../actions/types";
 
 const initialState = {
   info: {
     logo: null,
-    role: null
+    role: null,
+    companyColor: null
   }
-}
+};
 
 export const userInfoReducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case USER_INFO_FETCH_SUCCESS:
       return {
         ...state,
         info: {
           logo: action.payload.imgurl,
-          role: action.payload.role
+          role: action.payload.role,
+          companyColor: action.payload.companyColor
         }
-      }
+      };
     case USER_INFO_FETCH_FAILURE:
       return {
         ...state,
         info: null
-      }
+      };
     default:
       return state;
   }
-}
+};
