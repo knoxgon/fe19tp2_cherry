e472bf261d95c365d6d9c312866b02ab9e1836cf
jbrorson <josefine.brorson@gmail.com>
2020-02-12 11:50:52 +0100

    Added new info to payload

--- src/__redux/actions/userInfoActions.js
+++ src/__redux/actions/userInfoActions.js
@@ -1,29 +1,36 @@
 import { USER_INFO_FETCH_SUCCESS, USER_INFO_FETCH_FAILURE } from "./types";
 
-
 export const getInfo = () => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
     const firebase = getFirebase();
     const userid = firebase.auth().currentUser.uid;
-    firestore.collection('clients').doc(userid).get()
-      .then((res) => {
-        const { logo, role } = res.data();
-        firebase.storage().ref(logo).getDownloadURL().then((imgurl) => {
-          dispatch({
-            type: USER_INFO_FETCH_SUCCESS,
-            payload: {
-              imgurl: imgurl,
-              role: role
-            }
-          })
-        })
+    firestore
+      .collection("clients")
+      .doc(userid)
+      .get()
+      .then(res => {
+        const { logo, role, companyColor } = res.data();
+        firebase
+          .storage()
+          .ref(logo)
+          .getDownloadURL()
+          .then(imgurl => {
+            dispatch({
+              type: USER_INFO_FETCH_SUCCESS,
+              payload: {
+                imgurl: imgurl,
+                role: role,
+                companyColor: companyColor
+              }
+            });
+          });
       })
-      .catch((err) => {
+      .catch(err => {
         dispatch({
           type: USER_INFO_FETCH_FAILURE,
           payload: null
-        })
-      })
-  }
-}
+        });
+      });
+  };
+};
