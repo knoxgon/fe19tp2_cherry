2be604ab1956e72da3d69e7eb4dedb27e017106d
knoxgon <volkan.guven@hotmail.se>
2020-02-17 19:52:18 +0100

    Implementation of modal reducer

---
+++ src/__redux/reducers/modalReducer.js
@@ -0,0 +1,23 @@
+import { MODAL_ADD, MODAL_REMOVE } from '../actions/types';
+import random from 'randomstring';
+
+const initState = [{
+  dsid: random.generate(20)
+}]
+
+
+export const modalReducer = (state = initState, action) => {
+  switch(action.type) {
+    case MODAL_ADD:
+      return [
+        ...state, {
+          dsid: action.payload.dsid
+        }]
+    case MODAL_REMOVE:
+      if(state.length > 1)
+        return state.filter(elem => elem.dsid !== action.payload.dsid)
+      return state;
+    default:
+      return state;
+  }
+}
