1cb949eea4b7d64c1dc9e88a3e956f3cecf06b5d
knoxgon <volkan.guven@hotmail.se>
2020-02-18 23:21:31 +0100

    Updated state keys and moved render view to separate function

--- src/components/Graph/Candle/candle.js
+++ src/components/Graph/Candle/candle.js
@@ -3,28 +3,31 @@ import { connect } from 'react-redux'
 import Chart from 'react-apexcharts';
 import {options, optionsBar} from './options';
 
-const CandleGraph = ({idset, barid, excRed}) => {
+
+const CandleGraph = ({modalId, barid, exchange}) => {
+  const renderGraph = () => {
+    return exchange.map(({status, dsid, primary, alternate}, i) => {
+      if(status === 'ok' && dsid === modalId && alternate.series[0].data.length > 0) {
+        return <React.Fragment key={i}>
+          <Chart options={options(dsid)} series={primary.series} type="candlestick" height="350px" width="550px" />
+          <Chart options={optionsBar(dsid, barid)} series={alternate.series} type="bar"  height="150px" width="550px" />
+        </React.Fragment>
+      } else if(status === 'ok' && dsid === modalId) {
+        return <Chart key={i} options={options(dsid)} series={primary.series} type="candlestick" height="350px" width="550px" />
+      }
+      return null;
+    })
+  }
+
   return (
-    <React.Fragment>
-      {excRed.map((grafData, i) => {
-        if(grafData.status === 'ok' && grafData.dsid === idset && grafData.alternate.series[0].data.length > 0) {
-          return <React.Fragment key={i}>
-            <Chart options={options(grafData.dsid)} series={grafData.primary.series} type="candlestick" height="350px" width="550px" />
-            <Chart options={optionsBar(grafData.dsid, barid)} series={grafData.alternate.series} type="bar"  height="150px" width="550px" />
-          </React.Fragment>;
-        } else if(grafData.status === 'ok' && grafData.dsid === idset) {
-          return <Chart key={i} options={options(grafData.dsid)} series={grafData.primary.series} type="candlestick" height="350px" width="550px" />
-        }
-        return null;
-      })}
-    </React.Fragment>
+    renderGraph()
   )
 }
 
 const mapStateToProps = (state, props) => {
   return {
-    excRed: state.excRed,
-    idset: props.idset,
+    exchange: state.exchange,
+    modalId: props.modalId,
     barid: props.barid
   }
 }
