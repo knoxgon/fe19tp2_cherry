0cc2cadd036e59ed69bfe864a4a1b823a10ec4f2
knoxgon <volkan.guven@hotmail.se>
2020-02-18 13:03:06 +0100

    Component conversion to functional

--- src/__route/routes.js
+++ src/__route/routes.js
@@ -6,19 +6,17 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux';
 import AddClient from '../components/AddClient/addClient'
 
-class Routes extends React.Component {
-  render() {
-    return (
-      <Switch>
-        <PrivateRoute exact path="/account" component={Account} authenticated={this.props.auth.uid} />
-        <Route exact path="/solutions" />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/about" />
-        <Route exact path="/client" component={AddClient} />
-        <Redirect to="/login"></Redirect>
-      </Switch>
-    );
-  }
+const Routes = ({auth}) => {
+  return (
+    <Switch>
+      <PrivateRoute exact path="/account" component={Account} authenticated={auth.uid} />
+      <Route exact path="/solutions" />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/about" />
+      <Route exact path="/client" component={AddClient} />
+      <Redirect to="/login"></Redirect>
+    </Switch>
+  );
 }
 
 const mapStateToProps = (state) => {
