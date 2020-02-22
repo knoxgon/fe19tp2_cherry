f16c804273d8da3445bfa73afe8a4b66c1a97250
knoxgon <volkan.guven@hotmail.se>
2020-02-22 08:07:49 +0100

    Removed unused module

--- src/View/modalCandleView.js
+++ src/View/modalCandleView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { containerClose, containerCreate } from '../__redux/actions/containerAction';
 import random from 'randomstring';
