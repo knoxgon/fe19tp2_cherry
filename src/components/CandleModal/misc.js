be6abd22e4f53d7f2da19d52b633401513af6599
knoxgon <volkan.guven@hotmail.se>
2020-02-23 12:21:46 +0100

    Organized common file to a centralized conventional names

--- src/components/CandleModal/misc.js
+++
@@ -1,9 +0,0 @@
-export const parseDatePrev = (e) => {
-  e.setDate(e.getDate() - 1)
-  return parseInt((new Date(e).getTime()).toString().substring(0,  10))
-} 
-export const parseDate = (e) => parseInt((new Date(e).getTime()).toString().substring(0,  10))
-export const normDatePrev = (e) => {
-  const d = parseDatePrev(e);
-  return new Date(d * 1000)
-}
