d1acb04fab243452d5bc91fd8144c9e7d592de78
knoxgon <volkan.guven@hotmail.se>
2020-02-23 15:25:59 +0100

    Implemented modal triggering action for pie

--- src/__redux/actions/modalActions.js
+++ src/__redux/actions/modalActions.js
@@ -1,4 +1,4 @@
-import { CANDLE_MODAL_TOGGLE, LINE_MODAL_TOGGLE } from './types'
+import { CANDLE_MODAL_TOGGLE, LINE_MODAL_TOGGLE, PIE_MODAL_TOGGLE } from './types'
 
 
 export const fireCandleModalAction = () => {
@@ -13,6 +13,14 @@ export const fireLineModalAction = () => {
   return (dispatch, getState) => {
     dispatch({
       type: LINE_MODAL_TOGGLE
+    })
+  }
+}
+
+export const firePieModalAction = () => {
+  return (dispatch, getState) => {
+    dispatch({
+      type: PIE_MODAL_TOGGLE
     })
   }
 }
