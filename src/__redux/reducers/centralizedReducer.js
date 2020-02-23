89b385f68891dff133382396a7d3f114452eeba8
knoxgon <volkan.guven@hotmail.se>
2020-02-23 19:01:04 +0100

    Merged additional case to central factory

--- src/__redux/reducers/centralizedReducer.js
+++ src/__redux/reducers/centralizedReducer.js
@@ -1,10 +1,23 @@
-import { FETCH_CANDLE_EXCHANGE_SUCCESS, FETCH_CANDLE_EXCHANGE_FAILURE, CONTAINER_REMOVE, FETCH_EARNING_SURPRISES_SUCCESS, FETCH_EARNING_SURPRISES_FAILURE } from '../actions/types';
+import { FETCH_CANDLE_EXCHANGE_SUCCESS, FETCH_CANDLE_EXCHANGE_FAILURE, CONTAINER_REMOVE, FETCH_EARNING_SURPRISES_SUCCESS, FETCH_EARNING_SURPRISES_FAILURE, FETCH_RECOMMENTATION_TRENDS_SUCCESS, FETCH_RECOMMENTATION_TRENDS_FAILURE } from '../actions/types';
 
 const initState = []
 
-
 export const centralReducer = (state = initState, action) => {
   switch(action.type) {
+    case FETCH_RECOMMENTATION_TRENDS_SUCCESS:
+      return [
+        ...state,
+        {
+          gtype: 'pie',
+          dsid: action.payload.dsid,
+          series: [...action.payload.data],
+          status: 'ok',
+          period: action.payload.period,
+          compname: action.payload.compname
+        }
+      ]
+    case FETCH_RECOMMENTATION_TRENDS_FAILURE:
+      return state;
     case FETCH_EARNING_SURPRISES_SUCCESS:
       if(state.findIndex(e => e.dsid === action.payload.dsid) === -1) {
         return [
@@ -32,7 +45,9 @@ export const centralReducer = (state = initState, action) => {
             dsid: action.payload.dsid,
             status: action.payload.status,
             primary: {series: [{data: [...action.payload.primary]}]},
-            alternate: {series: [{data: [...action.payload.alternate]}]}
+            alternate: {series: [{data: [...action.payload.alternate]}]},
+            market: action.payload.market,
+            currency: action.payload.currency
           }
         ]
       } else {
@@ -44,7 +59,9 @@ export const centralReducer = (state = initState, action) => {
               dsid: elem.dsid,
               status: action.payload.status,
               primary: {series: [{data: [...action.payload.primary]}]},
-              alternate: {series: [{data: [...action.payload.alternate]}]}
+              alternate: {series: [{data: [...action.payload.alternate]}]},
+              market: action.payload.market,
+              currency: action.payload.currency
             }
           }
           return elem
