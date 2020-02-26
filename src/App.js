9886b3ca6e1a3924f0f4b577ac5f58cc9e7441ae
knoxgon <volkan.guven@hotmail.se>
2020-02-26 11:39:20 +0100

    Removed navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Navbar from "./components/Navbar/navbar";
 import Routes from './__route/routes';
 
 const StyledApp = styled.div`
