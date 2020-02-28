608a17d6bd45866f25ddcf7eacd23b751cd99516
knoxgon <volkan.guven@hotmail.se>
2020-02-28 11:16:50 +0100

    Stocks incoming theme package for direct transmission

--- src/__redux/reducers/darkModeReducer.js
+++ src/__redux/reducers/darkModeReducer.js
@@ -1,19 +1,44 @@
-import { DARKMODE_TOGGLE } from '../actions/types';
-import { ThemeColorsDark, ThemeColorsLight } from '../../__config/theme';
+import { DARKMODE_TOGGLE, USER_COLOR_PREP } from '../actions/types';
 const initialState = {
 	toggle: JSON.parse(localStorage.getItem('DarkMode')),
-	color: { colors: JSON.parse(localStorage.getItem('DarkMode')) ? ThemeColorsDark.colors : ThemeColorsLight.colors }
+	lightColors: {},
+	darkColors: {},
+	activeTheme: {}
 }
 
 export const darkModeReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case DARKMODE_TOGGLE:
 			const isDarkMode = !state.toggle
-			const color = isDarkMode ? ThemeColorsDark : ThemeColorsLight
+			const theme = isDarkMode ? state.darkColors : state.lightColors
 			return {
 				...state,
 				toggle: isDarkMode,
-				color
+				activeTheme: theme
+			}
+		case USER_COLOR_PREP:
+			const activeTheme = state.toggle ? {
+				contColor: action.payload.companyDarkContainerColor,
+				fontColor: action.payload.companyDarkFontColor,
+				navColor: action.payload.companyDarkNavbarColor
+			} : {
+				contColor: action.payload.companyLightContainerColor,
+				fontColor: action.payload.companyLightFontColor,
+				navColor: action.payload.companyLightNavbarColor
+			};
+			return {
+				...state,
+				activeTheme: activeTheme,
+				lightColors: { 
+					contColor: action.payload.companyLightContainerColor,
+					fontColor: action.payload.companyLightFontColor,
+					navColor: action.payload.companyLightNavbarColor
+				},
+				darkColors: {
+					contColor: action.payload.companyDarkContainerColor,
+					fontColor: action.payload.companyDarkFontColor,
+					navColor: action.payload.companyDarkNavbarColor
+				}
 			}
 		default:
 			return state;
