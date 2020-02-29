ed39545bbd8201988c0a628b637031a7cf237d1f
knoxgon <volkan.guven@hotmail.se>
2020-02-29 23:36:12 +0100

    Created SC to encapsulate graphic elements

--- src/components/View/styled.js
+++ src/components/View/styled.js
@@ -1,57 +1,37 @@
 import styled from 'styled-components'
 import Theme from '../../__config/theme';
+import { Resizable } from 're-resizable';
 
 export const MCCloser = styled.img`
   cursor: pointer;
-  position: relative;
-  width: 5rem;
-  height: 5rem;
   &:hover {
     transform: scale(1.3);
   }
 `;
 
-export const GMArea = styled.div`
-  position: relative;
+export const GMArea = styled(Resizable)`
+  width: 20rem;
+  margin-left: 2rem;
+  margin-top: 2rem;
+  height: 20rem;
   background-color: white;
-  margin: 3.5rem auto;
-  margin-left: 3.5rem;
-  width: 60rem;
-  height: 60rem;
   opacity: 0.9;
-  border: 1px solid #dddddd;
-  border-radius: 1rem;
-  box-shadow: -12px 10px 5px 0px rgba(221,221,221,1);
+  border-radius: 0.5rem;
   
-  @media screen and (max-width: ${Theme.screenSize.large}){
-    margin-left: 0rem;
-  }
-  @media screen and (max-width: ${Theme.screenSize.xsmall}) {
-    margin-left: 0rem;
-    height: 50rem;
-    width: 40rem;
-  }
-  transition: all .8s ease-in-out;
-  &:hover {
-    transform: scale(1.050);
-    @media screen and (max-width: ${Theme.screenSize.xsmall}) {
-      -webkit-transition: none;
-      transform: scale(1.0);
-    }
+  @media screen and (max-width: ${Theme.screenSize.small}){
+    margin-left: 0.25rem;
+    margin-top: 0.25rem;
   }
 `;
 
 export const GMTitle = styled.div`
-  position: relative;
-  font-size: 2.5rem;
   font-weight: 700;
   text-align: center;
-  padding-bottom: 5rem;
+  padding-bottom: 0.75rem;
   color: ${props => props.color};
 `;
 
 export const GMTop = styled.div`
-  position: relative;
   font-size: 2rem;
   font-weight: 900;
   text-align: right;
