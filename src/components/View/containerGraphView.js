4cb32b7d866e163d868394831469c695c301cb3f
knoxgon <volkan.guven@hotmail.se>
2020-03-01 00:01:28 +0100

    Emerged the adaptation of the central container

--- src/components/View/containerGraphView.js
+++ src/components/View/containerGraphView.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { containerClose } from '../../__redux/actions/containerActions';
+import { containerClose, brewsend } from '../../__redux/actions/containerActions';
 import random from 'randomstring';
-import { GMArea, MCCloser, GMTitle, GMTop } from "./styled"
+import { GMArea, MCCloser, GMTitle, GMTop, GMRH } from "./styled"
 import CandleGraph from '../Graph/Candle/candle'
 import LineGraph from '../Graph/Line/line'
 import PieGraph from '../Graph/Pie/pie'
 
-const ContainerGraphView = ({central, eraseContainer, containers}) => {
+const ContainerGraphView = ({brewort, central, eraseContainer, containers}) => {
   const containerOnDel = (dsid) => {
     eraseContainer(dsid)
   }
 
   const renderContainers = () => {
-    return containers.map(({dsid}, i) => {
+    return containers.map(({dsid, type, bsw, bsh, bsmw, bsmh, bslw, bslh}, i) => {
       return <React.Fragment key={i}>
-        <GMArea>
+        <GMArea t={type} bl={dsid} size={{ width: bsw, height: bsh }}
+          maxHeight={bsmh} maxWidth={bsmw} minHeight={bslh} minWidth={bslw}
+          enable={{ top:false, right:false, bottom:false, left:false, topRight:false, bottomRight:true, bottomLeft:false, topLeft:false }}
+          onResizeStop={(bz, dc, ac, lc) => {
+            brewort(dsid, lc.width, lc.height)
+          }}>
           <GMTop color="#0fc4ac">
             <MCCloser src={require('../../assets/employee/bin.svg')} onClick={() => containerOnDel(dsid)}></MCCloser>
           </GMTop>
           {central.map((grafData, j) => {
             if(grafData.status === 'ok' && grafData.dsid === dsid && grafData.gtype === 'candle') {
               return <React.Fragment key={j}>
-                <GMTitle color="#6c96af">OHLC</GMTitle>
+                <GMTitle color="#44b9bf">OHLC</GMTitle>
                 <CandleGraph market={grafData.market} currency={grafData.currency} primary={grafData.primary.series} alternate={grafData.alternate.series} containerId={dsid} barid={random.generate(16)}></CandleGraph>
                 </React.Fragment>
             } if(grafData.status === 'ok' && grafData.dsid === dsid && grafData.gtype === 'line') {
@@ -32,12 +37,11 @@ const ContainerGraphView = ({central, eraseContainer, containers}) => {
                 </React.Fragment>
             } if(grafData.status === 'ok' && grafData.dsid === dsid && grafData.gtype === 'pie') {
               return <React.Fragment key={j}>
-                <GMTitle color="#44b9bf">Recommendation Trends</GMTitle>
-                <PieGraph series={grafData.series} period={grafData.period} compname={grafData.compname} containerId={dsid}></PieGraph>
-                </React.Fragment>
-            }
-            return null;
+                <GMTitle color="#6c96af">{grafData.compname} - {grafData.period}</GMTitle>
+                <PieGraph series={grafData.series} containerId={dsid}></PieGraph>
+                </React.Fragment>}return null;
           })}
+          <GMRH/>
         </GMArea>
       </React.Fragment>
     })
@@ -56,7 +60,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    eraseContainer: (id) => dispatch(containerClose(id))
+    eraseContainer: (id) => dispatch(containerClose(id)),
+    brewort: (i, a, b) => dispatch(brewsend(i, a, b))
   }
 }
 
