b710bb2d05fd1e4c93600cc5822bcc10da2b8e3e
knoxgon <volkan.guven@hotmail.se>
2020-02-29 23:56:43 +0100

    Allocating incoming data for instant use

--- src/__redux/reducers/containerReducer.js
+++ src/__redux/reducers/containerReducer.js
@@ -1,4 +1,4 @@
-import { CONTAINER_ADD, CONTAINER_REMOVE, CONTAINER_REMOVE_LAST } from '../actions/types';
+import { CONTAINER_ADD, CONTAINER_REMOVE, CONTAINER_BREWSEND, CONTAINER_REMOVE_LAST } from '../actions/types';
 
 const initState = []
 
@@ -7,12 +7,25 @@ export const containerReducer = (state = initState, action) => {
     case CONTAINER_ADD:
       return [
         ...state, {
-        dsid: action.payload.dsid
+        dsid: action.payload.dsid,
+        type: action.payload.type,
+        bsw: action.payload.bsw,
+        bsh: action.payload.bsh,
+        bsmw: action.payload.bsmw,
+        bsmh: action.payload.bsmh,
+        bslw: action.payload.bslw,
+        bslh: action.payload.bslh
       }]
     case CONTAINER_REMOVE:
       return state.filter(elem => elem.dsid !== action.payload.dsid)
     case CONTAINER_REMOVE_LAST:
       return state.shift()
+    case CONTAINER_BREWSEND:
+      return state.map(obj => {
+        if(obj.dsid === action.payload.dsid) {
+          return { ...obj, bsw: obj.bsw + action.payload.bsw, bsh: obj.bsh + action.payload.bsh}
+        } return obj;
+      })
     default:
       return state;
   }
