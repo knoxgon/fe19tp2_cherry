64866699d71cf98445101e5bdb6e8aa2ef573cd5
knoxgon <volkan.guven@hotmail.se>
2020-03-03 11:51:15 +0100

    Created new device viewport mapping for responsiveness of the wizard tool

--- src/__config/theme.js
+++ src/__config/theme.js
@@ -11,6 +11,15 @@ export const ToggleDarkMode = () => {
 }
 
 const Theme = {
+  deviceSize: {
+    iph5SE: {w: '320px', h: '568px', offsetw: '359px'},
+    galaxy: {w: '360px', h: '640px', offsetw: '374px'},
+    iph678X: {w: '375px', h: '667px', offsetw: '413px'},
+    iph678P: {w: '414px', h: '736px', offsetw: '479px'},
+    nokia: {w: '480px', h: '854px', offsetw: '599px'},
+    nexus: {w: '600px', h: '960px', offsetw: '766px'},
+    ipad: {w: '768px', h: '1024px', offsetw: '767px'}
+  },
   screenSize: {
     xsmall: '600px',
     small: '768px',
