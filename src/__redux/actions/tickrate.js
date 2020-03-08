6c13100f3728b7c2da493c5a3b8ea5baad899227
knoxgon <volkan.guven@hotmail.se>
2020-03-08 21:19:56 +0100

    Implemented default tick action upon first launch

--- src/__redux/actions/tickrate.js
+++ src/__redux/actions/tickrate.js
@@ -32,3 +32,43 @@ export const tick = (compname) => {
     }
   }
 }
+
+export const tickUpdate = (id, compname, interval) => {
+  return (dispatch, getState) => {
+    if (!compname) {
+      throw new Error('Please select a company')
+    } else {
+      let per = day5();
+      let resolut = '60';
+      switch(interval) {
+        case 'y50': per = year50(); resolut = 'W'; break;
+        case 'y5': per = year5(); resolut = 'W'; break;
+        case 'y1': per = year1(); resolut = 'D'; break;
+        case 'm6': per = month6(); resolut = 'D'; break;
+        case 'm1': per = month1(); resolut = 'D'; break;
+        case 'd5': per = day5(); resolut = '30'; break;
+        default: per = day5(); resolut = '30'; break;
+      }
+      Axios(`https://finnhub.io/api/v1/stock/candle?symbol=${compname.value}&resolution=${resolut}&from=${per}&to=${currentTime()}&token=bpib37nrh5rbgl0l9l70`)
+      .then(result => {
+        if(result.data.s !== 'ok') throw new Error('No record available')
+        const data = result.data.c.map((a, i) => Array.of(result.data.t[i] * 1000, a))
+        let containerId = getState().containers.find(ctr => ctr.dsid === id).dsid
+        dispatch({
+          type: FETCH_TICKRATE_UPDATE_SUCCESS,
+          payload: {
+            dsid: containerId,
+            data,
+            status: result.data.s,
+            compname: compname
+          }
+        })
+      }).catch(err => {
+        dispatch({
+          type: FETCH_TICKRATE_UPDATE_FAILURE
+        })
+      })
+    }
+  }
+}
+
