eec477090e1cd305681ebb47e5fe43efb2d1a18b
jbrorson <josefine.brorson@gmail.com>
2020-03-09 14:21:37 +0100

    Admin user type added

--- src/__redux/actions/types.js
+++ src/__redux/actions/types.js
@@ -34,3 +34,5 @@ export const QTC = 'QTC';
 export const QTP = 'QTP';
 export const QTL = 'QTL';
 export const QT = 'QT';
+export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS';
+export const GET_USERS_FAILURE = 'GET_USERS_FAILURE';
