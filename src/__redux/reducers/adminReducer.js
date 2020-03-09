f675faf4e3a6380f76da6e5f4e310ea7048d3dc3
jbrorson <josefine.brorson@gmail.com>
2020-03-09 14:31:29 +0100

    Invoked admin reducer

---
+++ src/__redux/reducers/adminReducer.js
@@ -0,0 +1,17 @@
+import { GET_USERS_FAILURE, GET_USERS_SUCCESS } from "../actions/types";
+
+const initState = {}
+
+export const adminReducer = (state = initState, action) => {
+  switch(action.type) {
+    case GET_USERS_SUCCESS:
+      return {
+        ...state,
+        users: [...action.payload.users]
+      }
+    case GET_USERS_FAILURE:
+      return state;
+    default:
+      return state;
+  }
+}
