e5b4b7e7905dd8f67daee204ffa17ca8b5aa8be0
knoxgon <volkan.guven@hotmail.se>
2020-03-10 13:24:04 +0100

    Design applied to registration view

--- src/components/AddEmployee/addEmployee.js
+++ src/components/AddEmployee/addEmployee.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addEmployee } from '../../__redux/actions/employeeActions';
-import { EmployeeForm, EmployeeInput, Button } from '../AddEmployee/styledAddEmployee';
- 
+import { EmployeeForm, EmployeeInput, Button, FormArea, FormDescription, FormFieldset, AError } from '../AddEmployee/styledAddEmployee';
+
 const AddEmployee = (props, theme) => {
   const [info, setInfo] = useState({ email: '', password: '', firstname: '', lastname: '' })
 
@@ -10,26 +10,24 @@ const AddEmployee = (props, theme) => {
     e.preventDefault();
     props.makeEmployee(info);
   }
-
   const onInputChangeHandler = (e) => {
     const { name, value } = e.target;
     setInfo({...info, [name]: value});
   }
-
   return (
+    <FormArea>
       <EmployeeForm onSubmit={ onEmployeeRegistration }>
-        <EmployeeInput placeholder="Email" type="email" name="email" onChange={onInputChangeHandler} autoComplete="new-password"></EmployeeInput>
-        <br/>
-        <EmployeeInput placeholder="Password" type="password" name="password" onChange={onInputChangeHandler}></EmployeeInput>
-        <br/>
-        <EmployeeInput placeholder="Firstname" type="text" name="firstname" onChange={onInputChangeHandler}></EmployeeInput>
-        <br/>
-        <EmployeeInput placeholder="Lastname" type="text" name="lastname" onChange={onInputChangeHandler}></EmployeeInput>
-        <br/>
-        {props.feedback ? <div>Message: {props.feedback}</div> : null }
-        <br/>
-        <Button btcolor={theme.navColor} type="submit">Add</Button>
+        <FormFieldset>
+          <FormDescription>Register user</FormDescription>
+          <EmployeeInput placeholder="Email" type="email" name="email" onChange={onInputChangeHandler} autoComplete="new-password"></EmployeeInput>
+          <EmployeeInput placeholder="Password" type="password" name="password" onChange={onInputChangeHandler}></EmployeeInput>
+          <EmployeeInput placeholder="Firstname" type="text" name="firstname" onChange={onInputChangeHandler}></EmployeeInput>
+          <EmployeeInput placeholder="Lastname" type="text" name="lastname" onChange={onInputChangeHandler}></EmployeeInput>
+          {props.feedback ? <AError>{props.feedback}</AError> : null }
+          <Button btcolor={theme.navColor} type="submit">Add</Button>
+        </FormFieldset>
       </EmployeeForm>
+    </FormArea>
   );
 }
 
--- src/components/AddEmployee/styledAddEmployee.js
+++ src/components/AddEmployee/styledAddEmployee.js
@@ -1,41 +1,59 @@
 import styled from 'styled-components'
 import Theme from "../../__config/theme";
 
-export const EmployeeForm = styled.form`
-display: flex;
-flex-direction: column;
-width: 20rem;
-height: 10rem;
-justify-content: center;
-align-items: center;
-margin-left: 10rem;
-margin-top: 8rem;
-
-  @media screen and (max-width: ${Theme.screenSize.xsmall}){
-    input{
-      width: 90%;
-      margin-left: 1rem;
-      margin-bottom: 1rem;
-    }
-    button{
-      display: block;
-      margin: auto;
-      background-color: black;
-    }
+export const FormArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 35vw;
+  height: fit-content;
+  @media screen and (max-width: ${Theme.deviceSize.ipad.w}){
+    width: 95vw;
   }
 `;
 
+export const FormDescription = styled.legend`
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: blue;
+`;
+
+export const FormFieldset = styled.fieldset`
+  padding: 5rem;
+  border-color: #d4e1ff;
+  @media screen and (max-width: ${Theme.deviceSize.nokia.w}){
+    padding: 0rem;
+  }
+`;
+
+export const EmployeeForm = styled.form`
+  text-align: center;
+`;
+
 export const EmployeeInput = styled.input`
-padding: 1rem;
-width: 30rem;
-background: ${props => props.fcolor};
+  padding: 1rem;
+  width: 30rem;
+  background: ${props => props.fcolor};
+`;
+
+export const AError = styled.div`
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  color: blue;
+  font-weight: 700;
 `;
 
 export const Button = styled.button`
-align-items: center;
-width: 8rem; 
-background-color: ${props => props.btcolor};
-cursor: pointer;
+  align-items: center;
+  width: 50%;
+  cursor: pointer;
+  border: none;
+  margin-top: 2.5rem;
+  background-color: #b9c8e6;
+  height: 3rem;
+  color: #5858bd;
+  font-size: 1.75rem;
+  font-weight: 600;
+  border-radius: 30px;
 `;
 
 
