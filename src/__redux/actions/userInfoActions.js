e924c3c47b81de746845ff2967b89999fb8e67f7
knoxgon <volkan.guven@hotmail.se>
2020-03-10 13:18:37 +0100

    Removal of usars and info extraction enhanced

--- src/__redux/actions/userInfoActions.js
+++ src/__redux/actions/userInfoActions.js
@@ -1,4 +1,4 @@
-import { USER_INFO_FETCH_SUCCESS, USER_COLOR_PREP, USER_INFO_FETCH_FAILURE, GET_USERS_FAILURE, GET_USERS_SUCCESS } from "./types";
+import { USER_INFO_FETCH_SUCCESS, USER_COLOR_PREP, USER_INFO_FETCH_FAILURE, GET_USERS_FAILURE, GET_USERS_SUCCESS, REMOVE_USER_SUCCESS, REMOVE_USER_FAILURE } from "./types";
 
 export const getInfo = () => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
@@ -12,6 +12,11 @@ export const getInfo = () => {
       });
     } else {
       const userid = firebase.auth().currentUser.uid;
+      firestore.collection("clients").doc(userid).get().then(e => {
+        if(!e.exists) {
+          firebase.auth().currentUser.delete()
+        }
+      })
       firestore
       .collection("clients")
       .doc(userid)
@@ -76,36 +81,57 @@ export const getUsers = () => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
     const firebase = getFirebase();
-
-    if(!firebase.auth().currentUser) {                                                
+    const admin_uid = firebase.auth().currentUser.uid;
+    firestore
+    .collection("clients")
+    .get()
+    .then(res => {
+      const users = res.docs.map(elem => {
+        if( elem.data().adminId === admin_uid){
+          return { fullname: elem.data().firstname + ' ' + elem.data().lastname, role: elem.data().role, id: elem.id}}
+        return undefined;
+      }).filter(elem => elem)
+      dispatch({
+        type: GET_USERS_SUCCESS,
+        payload: {
+          users
+        }
+      })
+    })
+    .catch(err => {
       dispatch({
         type: GET_USERS_FAILURE,
         payload: null
       });
-    } else {
-      const admin_uid = firebase.auth().currentUser.uid;
-      firestore
-      .collection("clients")
-      .get()
-      .then(res => {
-        const users = res.docs.map(elem => {
-          if( elem.data().adminId === admin_uid)
-            return { fullname: elem.data().firstname + ' ' + elem.data().lastname, role: elem.data().role}
-        }).filter(elem => elem)
-        dispatch({
-          type: GET_USERS_SUCCESS,
-          payload: {
-            users
-          }
-        })
-      })
-      .catch(err => {
-        dispatch({
-          type: GET_USERS_FAILURE,
-          payload: null
-        });
-      });
-    }
+    });
   };
 };
 
+export const removeUser = (id) => {
+  return(dispatch, getState, {getFirebase, getFirestore}) => {
+    const firestore = getFirestore();
+    const firebase = getFirebase();
+    const uid = firebase.auth().currentUser.uid
+    const clientsRef = firestore.collection('clients').doc(uid)
+    firestore.runTransaction((tr) => {
+      return tr.get(clientsRef).then((dc) => {
+        const newSet = dc.data().users.filter(a => a.employeeReference !== id)
+        tr.update(clientsRef, {users: newSet})
+      })
+    }).then(() => {
+      firestore.collection("clients").doc(id).delete().then(() => {
+        dispatch({
+          type: REMOVE_USER_SUCCESS,
+          payload: 'Success'
+        })
+        dispatch(getUsers());
+      })
+      .catch(err => {
+        dispatch({
+          type: REMOVE_USER_FAILURE,
+          payload: 'Failed'
+        });
+      });
+    })
+  }
+}
